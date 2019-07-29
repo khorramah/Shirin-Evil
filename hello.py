241d9b5b3892fcde4f144c344e4f607a39c54979
khorramah <53340996+khorramah@users.noreply.github.com>
2019-07-29 15:18:57 +0430

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-print('helloworld')
-a = input('Enter Your Target: ')
-print(a)
+import sys
+import os
+import time 
+load = '='
+for _ in range(60):
+  os.system('clear')
+  print(load)
+  time.sleep(1)
